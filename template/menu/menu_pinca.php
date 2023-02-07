d7c738b1ebcf2957c9b4e11dc55eeb75ec889bc3
Yeremia Wahyu <yeremiawahyu009@gmail.com>
2023-02-07 21:05:55 +0700

    fix tampilan menu

--- template/menu/menu_pinca.php
+++ template/menu/menu_pinca.php
@@ -3,11 +3,10 @@
     <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
     <li class="nav-item menu-open">
-      <a href="#" class="nav-link active">
+      <a href="#" class="nav-link">
         <i class="nav-icon fas fa-tachometer-alt"></i>
         <p>
           Dashboard
-          <i class="right fas fa-angle-left"></i>
         </p>
       </a>
     </li>
