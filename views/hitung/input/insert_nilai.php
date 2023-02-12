48fb02cb3680bda7546e93cc631f995d428772f6
Yeremia Wahyu <yeremiawahyu009@gmail.com>
2023-02-13 01:53:00 +0700

    edit view

--- views/hitung/input/insert_nilai.php
+++ views/hitung/input/insert_nilai.php
@@ -9,11 +9,6 @@ $id_isi = $_POST['id_isi'];
 $nilai = $_POST['nilai'];
 $count = count($nilai);
 
-// var_dump($id_pegawai);
-// var_dump($id_isi);
-// var_dump($nilai);
-// var_dump($count);
-
 for($i=0; $i <= $count; $i++){
   $insert = mysqli_query($conn, "INSERT INTO hitung_nilai VALUES ('','{$id_pegawai[$i]}','{$id_isi[$i]}','{$nilai[$i]}')");
 }
