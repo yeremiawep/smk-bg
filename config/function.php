2a7922bbeb8cdc4a223a4156a5c6efaeeb957718
yeremia wahyu <yeremiawahyu009@gmail.com>
2023-02-21 19:41:58 +0700

    edit function

--- config/function.php
+++ config/function.php
@@ -1,5 +1,8 @@
 <?php
 
+// Pemberian Predikat 
+
+// ------------- Start ----------------- //
 function predikat($nilai_akhir)
 {
 
@@ -19,7 +22,64 @@ function predikat($nilai_akhir)
 
   return $p;
 }
+// ----------------- End ------------------- //
 
-function hitung_sk()
+
+// Menghitung nilai sasaran kompetensi untuk level non manajerial
+
+// ------------- Start ----------------------------------------- //
+function hitung_sk_plk($nilaisk, $countsk)
 {
+  $total_nilai_sk = array_sum($nilaisk) / $countsk * 40 / 100;
+
+  return $total_nilai_sk;
 }
+// ------------- End ------------------------------------------- //
+
+
+// Menghitung nilai sasaran kinerja objektif untuk level non manajerial
+
+// ------------- Start ----------------------------------------- //
+function hitung_nilai_sko_plk($nilaisko, $count)
+{
+  $total_nilai_sko = array_sum($nilaisko) / $count * 60 / 100;
+
+  return $total_nilai_sko;
+}
+// ---------------------- End ------------------------------------ //
+
+
+// Menghitung nilai sasaran kinerja objektif untuk level manajerial
+
+// ---------------------- Start ---------------------------------- //
+function hitung_nilai_sko_mnj($nilaisko, $count)
+{
+  $total_nilai_sko = array_sum($nilaisko) / $count * 70 / 100;
+
+  return $total_nilai_sko;
+}
+// ----------------------- End ------------------------------------ //
+
+
+// Menghitung nilai sasaran kompetensi untuk level manajerial 
+
+// ------------------------- Start -------------------------------- //
+function hitung_nilai_sk_mnj($nilaisk, $countsk)
+{
+  $total_nilai_sk = array_sum($nilaisk) / $countsk * 30 / 100;
+
+  return $total_nilai_sk;
+}
+// ------------------------- End ---------------------------------- //
+
+
+// Menghitung Nilai Akhir
+
+// ---------------------------- Start ----------------------------- //
+function hitung_nilai_akhir($total_nilai_sko, $total_nilai_sk, $nilaihk)
+{
+  $nilai_akhir = $total_nilai_sko + $total_nilai_sk - $nilaihk;
+
+  return $nilai_akhir;
+}
+// ---------------------------- End ------------------------------- //
