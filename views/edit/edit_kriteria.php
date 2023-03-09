1598f3e6e9e083c25690447ff432089c35bf2a22
Yeremia Wahyu <yeremiawahyu009@gmail.com>
2023-03-09 23:53:10 +0700

    edit kriteria

--- views/edit/edit_kriteria.php
+++ views/edit/edit_kriteria.php
@@ -1,65 +1,5 @@
 <?php
 
-$id = $_GET['id'];
-$kriteria = mysqli_query($conn, "SELECT * FROM kriteria_penilaian JOIN jabatans ON kriteria_penilaian.jabatan=jabatans.id WHERE id_isi_sko = '$id'");
-
-?>
-<!-- 
-<section class="content">
-  <div class="container-fluid">
-    <div class="card card-primary">
-      <div class="card-header">
-        <h4 class="card-title">Edit Data Kriteria</h4>
-      </div>
-<div class="card-body">
-  <form method="POST" action="../views/update/update_data_kriteria.php">
-    <?php foreach ($kriteria as $k) : ?>
-      <div class="row">
-        <input type="text" name="id_isi" value="<?= $k['id_isi_sko']; ?>" hidden>
-      </div>
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="form-group">
-            <label for="jabatan">Jabatan</label>
-            <input type="text" class="form-control" name="jabatan" value="<?= $k['name_jab']; ?>" readonly>
-          </div>
-        </div>
-      </div>
-      <div class=" row">
-        <div class="col-sm-6">
-          <div class="form-group">
-            <label>Aspek</label>
-            <input type="text" class="form-control" name="aspek" value="<?= $k['aspek']; ?>">
-          </div>
-        </div>
-      </div>
-      <div class=" row">
-        <div class="col-sm-6">
-          <div class="form-group">
-            <label>Kriteria</label>
-            <input type="text" class="form-control" name="kriteria" value="<?= $k['kriteria']; ?>">
-          </div>
-        </div>
-      </div>
-      <div class=" row">
-        <div class="col-sm-6">
-          <div class="form-group">
-            <label>Target</label>
-            <input type="text" class="form-control" name="target" value="<?= $k['target']; ?>">
-          </div>
-        </div>
-      </div>
-    <?php endforeach; ?>
-    <a href="index.php?page=data-kinerja-objektif" class="btn btn-sm btn-warning">Back</a>
-    <button type="submit" class="btn btn-sm btn-primary">Submit</button>
-  </form>
-</div>
-</div>
-</div>
-</section> -->
-
-<?php
-
 include '../config/database.php';
 
 $jab = $_GET['jab'];
@@ -68,7 +8,6 @@ $tkkep = mysqli_query($conn, "SELECT * FROM tk_kepentingan");
 $kriteria = mysqli_query($conn, "SELECT * FROM kriteria_penilaian JOIN tk_kepentingan ON kriteria_penilaian.id_tk=tk_kepentingan.id_tk WHERE jabatan='$jab'");
 $jabatan = mysqli_query($conn, "SELECT * FROM jabatans WHERE id='$jab'");
 $j = $jabatan->fetch_array();
-// $jml = mysqli_query($conn, "SELECT COUNT(kriteria) FROM kriteria_penilaian WHERE jabatan='$jab'");
 
 ?>
 
--- views/update/update_data_kriteria.php
+++ views/update/update_data_kriteria.php
@@ -2,11 +2,6 @@
 include '../../config/database.php';
 session_start();
 
-// $aspek = $_POST['aspek'];
-// $kriteria = $_POST['kriteria'];
-// $target = $_POST['target'];
-// $jab = $_POST['jab'];
-
 $id_isi = $_POST['id_isi'];
 $aspek = $_POST['aspek'];
 $kriteria = $_POST['kriteria'];
@@ -20,12 +15,6 @@ for ($i = 0; $i < $ctkp; $i++) {
   $bobot[$i] = $tk_kep[$i] / $tkkep;
 }
 
-// print_r($id_isi);
-// print_r($aspek);
-// print_r($kriteria);
-// print_r($target);
-// print_r($tk_kep);
-// print_r($bobot);
 
 for ($i = 0; $i < $ctkp; $i++) {
   $query = mysqli_query($conn, "UPDATE kriteria_penilaian SET aspek='$aspek[$i]', kriteria='$kriteria[$i]', target='$target[$i]', id_tk='$tk_kep[$i]', bobot='$bobot[$i]' WHERE id_isi_sko='$id_isi[$i]'");
@@ -36,6 +25,5 @@ if ($query) {
 } else {
   $_SESSION['gagal'] = 'Failed';
 }
-
 
 header('Location: ../../app/index.php?page=data-kinerja-objektif');
